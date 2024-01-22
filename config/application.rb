906e3820a76d344ba21fa3a39b85898da85821e2
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-01-22 23:46:31 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,36 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ProjectSample
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+    config.generators do |g|
+      # helperファイルの設定
+      # testファイルの設定
+      # ルーティングの記述の設定
+      g.skip_routes true
+      g.helper false
+      g.test_framework nil
+
+    end
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
